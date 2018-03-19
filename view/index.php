0128256e5245618489ca9a21e0a5d2ceca642e7d
antonmih <toni.mikheykin@mail.ru>
2018-03-19 12:12:56 +0700

    create form stavka

---
+++ view/index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>
+            Azino 777
+        </title>
+    </head>
+    <body>
+        Balans: 10
+        <form action="" method="post">
+        <input type="text" name="stavka1">
+        <input type="text" name="stavka2">
+        <input type="button" name="post" value="Postavit">
+        </form>
+    </body>
+</html>
